a6db9b0ecdd318888195db11685df2982716d64d
zamboni <luiz.zamboni@zoom.com.br>
2020-05-10 18:40:34 -0300

    ADD nginx ingress provision

--- provisions/master/nginx-ingress.sh
+++ provisions/master/nginx-ingress.sh
@@ -1,35 +1,31 @@
 #!/bin/bash
 
-git clone https://github.com/nginxinc/kubernetes-ingress/
-git checkout v1.7.0
-
-
 # namespace and account service
-kubectl apply -f kubernetes-ingress/deployments/common/ns-and-sa.yaml
+kubectl apply -f $vagrant_dir/manifests/nginx-ingress/deployments/common/ns-and-sa.yaml
 
 # cluster role
-kubectl apply -f kubernetes-ingress/deployments/rbac/rbac.yaml
+kubectl apply -f $vagrant_dir/manifests/nginx-ingress/deployments/rbac/rbac.yaml
 
 # secret with tls cert data
-kubectl apply -f kubernetes-ingress/deployments/common/default-server-secret.yaml
+kubectl apply -f $vagrant_dir/manifests/nginx-ingress/deployments/common/default-server-secret.yaml
 
 
 # Create a config map for customizing NGINX configuration:
-kubectl apply -f kubernetes-ingress/deployments/common/nginx-config.yaml
+kubectl apply -f $vagrant_dir/manifests/nginx-ingress/deployments/common/nginx-config.yaml
 
 # Create custom resource definitions for VirtualServer and VirtualServerRoute and TransportServer resources
-kubectl apply -f kubernetes-ingress/deployments/common/vs-definition.yaml
-kubectl apply -f kubernetes-ingress/deployments/common/vsr-definition.yaml
-kubectl apply -f kubernetes-ingress/deployments/common/ts-definition.yaml
+kubectl apply -f $vagrant_dir/manifests/nginx-ingress/deployments/common/vs-definition.yaml
+kubectl apply -f $vagrant_dir/manifests/nginx-ingress/deployments/common/vsr-definition.yaml
+kubectl apply -f $vagrant_dir/manifests/nginx-ingress/deployments/common/ts-definition.yaml
 
 
 # if you would like to use the TCP and UDP load balancing features of the Ingress Controller, create the following additional resources:
 
 # Create a custom resource definition for GlobalConfiguration resource:
-kubectl apply -f kubernetes-ingress/deployments/common/gc-definition.yaml
+kubectl apply -f $vagrant_dir/manifests/nginx-ingress/deployments/common/gc-definition.yaml
 
 # Create a GlobalConfiguration resource:
-kubectl apply -f kubernetes-ingress/deployments/common/global-configuration.yaml
+kubectl apply -f $vagrant_dir/manifests/nginx-ingress/deployments/common/global-configuration.yaml
 
 
 
@@ -40,13 +36,13 @@ kubectl apply -f kubernetes-ingress/deployments/common/global-configuration.yaml
 
 ## Use a DaemonSet: When you run the Ingress Controller by using a DaemonSet, Kubernetes will create an Ingress controller pod on every node of the cluster.
 ### See also: See the Kubernetes DaemonSet docs to learn how to run the Ingress controller on a subset of nodes instead of on every node of the cluster.
-kubectl apply -f kubernetes-ingress/deployments/daemon-set/nginx-ingress.yaml
-kubectl create -f kubernetes-ingress/deployments/service/nodeport.yaml
+kubectl apply -f $vagrant_dir/manifests/nginx-ingress/deployments/daemon-set/nginx-ingress.yaml
+kubectl create -f $vagrant_dir/manifests/nginx-ingress/deployments/service/nodeport.yaml
 
 
 
 # coffe example
-kubectl apply -f kubernetes-ingress/examples/complete-example/cafe.yaml
-kubectl apply -f kubernetes-ingress/examples/complete-example/cafe-secret.yaml
-kubectl apply -f kubernetes-ingress/examples/complete-example/cafe-ingress.yaml
+kubectl apply -f $vagrant_dir/manifests/nginx-ingress/examples/complete-example/cafe.yaml
+kubectl apply -f $vagrant_dir/manifests/nginx-ingress/examples/complete-example/cafe-secret.yaml
+kubectl apply -f $vagrant_dir/manifests/nginx-ingress/examples/complete-example/cafe-ingress.yaml
 
