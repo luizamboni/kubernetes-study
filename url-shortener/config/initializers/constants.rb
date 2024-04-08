53c221f687bbe7bd5458083bcdcbdec1796487c1
Luiz Zamboni <luizamboni2002@hotmail.com>
2024-04-07 23:13:10 -0300

    adding try/rescue when /etc/hostname is not available

--- url-shortener/config/initializers/constants.rb
+++ url-shortener/config/initializers/constants.rb
@@ -1,3 +1,7 @@
-f = open('/etc/hostname')
-INTERNAL_HOSTNAME = f.read()
-f.close()
+begin
+    f = open('/etc/hostname')
+    INTERNAL_HOSTNAME = f.read()
+    f.close()    
+rescue
+    INTERNAL_HOSTNAME = ""
+end
